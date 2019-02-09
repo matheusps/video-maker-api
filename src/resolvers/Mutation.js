9a921c4b3a22501cdd4a4bd33235b6f304683ea6
matheusps <matheus.ps@icloud.com>
2019-02-09 09:25:33 -0300

    User login

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -13,4 +13,19 @@ const signup = async (parent, args, context, info) => {
     }
 }
 
-export { signup }
+const login = async (parent, args, context, info) => {
+    const user = await context.prisma.user({ email: args.email })
+    if(!user) throw new Error('❌ No such user found!')
+
+    const valid = await bcrypt.compare(args.password, user.password)
+    if(!valid) throw new Error('❌ Invalid password!')
+
+    const token = jwt.sign({ userId: user.id}, APP_SECRET)
+
+    return {
+        token,
+        user,
+    }   
+}
+
+export { signup, login }
